8110e3a700606b2c640bbc168fe3a7e5d77120d7
михаил галев <mgalev257@gmail.com>
2018-06-22 20:14:20 +0300

    Import project

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DAreaofRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double a = x1 - y1;
+            double b = x2 - y2;
+            double perimeter = 2 * (a + b);
+            double area = a * b;
+
+            Console.WriteLine(area);
+            Console.WriteLine(perimeter);
+        }
+    }
+}
